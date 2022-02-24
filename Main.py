20564bc1f4461f994cadd133619fd55bfd85f261
sboese1 <samboese3@gmail.com>
2022-02-23 22:21:17 -0600

    Displays the notes added via a database if the using presses the notes button

--- Main.py
+++ Main.py
@@ -10,32 +10,53 @@ def create_note():
     note_text = notes.get('1.0', END)  # Gets all the text from the notes entry
 
     cur.execute("INSERT INTO main_notes VALUES (?, ?)", (title_text.strip(), note_text.strip()))  # Inserts the title and notes entries into the title and note spot in the database
-    for i in cur.execute('SELECT * FROM main_notes'):
-        print(i)
 
 
 root = Tk()  # Initializes the tkinter object
-frame = Frame(root)  # Constructs a frame object
+main_frame = Frame(root)  # Constructs a frame object
+notes_frame = Frame(root)
+root.geometry("400x750")
 root.resizable(False, False)  # User can't resize the window
 
 title = Text(root, bd=2, height=1, width=35)  # Creates a text box
 title.pack()
 
-notes = Text(root, bd=2, height=50, width=50)  # Creates a text box
+notes = Text(root, bd=2, height=44, width=50)  # Creates a text box
 notes.pack()
 
 submit_button = Button(root, text='Submit', command=create_note)  # Creates a button
 submit_button.pack()
 
+buttons = []
 
 def open_menu():
     title.pack_forget()  # Hides the title entry box
     notes.pack_forget()  # Hides the notes entry box
     submit_button.pack_forget()  # Hides the submit button
 
+    count = 0
+    for i in cur.execute('SELECT * FROM main_notes'):
+        count = count + 1
 
-menu = Button(root, text='Notes', height=1, bd=0, command=open_menu)  # Creates a button
+    for index, i in enumerate(cur.execute('SELECT * FROM main_notes')):
+        buttons.append(Button(root, text='Note' + str(i[0]), height=1, bd=0, command=open_menu))
+        buttons[index].grid(row=index, column=0, columnspan=5)
+
+
+def open_main():
+    for i in buttons:
+        i.grid_forget()
+
+    title.pack()  # Hides the title entry box
+    notes.pack()  # Hides the notes entry box
+    submit_button.pack()  # Hides the submit button
+
+
+menu = Button(root, text='Notes', height=1, bd=0, command=open_menu)  # Creates a menu button
 menu.place(x=12, y=0)  # Places the menu button at a certain position
+
+back_button = Button(root, text='Back', height=1, bd=0, command=open_main)  # Creates a back button
+back_button.place(x=355, y=0)  # Places the back button at a certain position
 
 mainloop()
 
